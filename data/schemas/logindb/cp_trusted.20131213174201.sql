cfcae0f4466450884ad4ed8d6253191b87574f1c
zdurexz <zdurexz@hotmail.com>
2020-05-04 16:21:11 +0700

    latin1 to utf8

--- data/schemas/logindb/cp_trusted.20131213174201.sql
+++ data/schemas/logindb/cp_trusted.20131213174201.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS `cp_trusted` (
   `delete_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `account_id` (`account_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
--- data/schemas/logindb/cp_txnlog.20170221184601.sql
+++ data/schemas/logindb/cp_txnlog.20170221184601.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `cp_txnlog` CHANGE `address_status` `address_status` VARCHAR(11) CHARACTER SET latin1 COLLATE utf8_general_ci NULL DEFAULT NULL;
+ALTER TABLE `cp_txnlog` CHANGE `address_status` `address_status` VARCHAR(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
